d0c8976082eebc9a1a72f911943e7d76f59ce045
PeterBartko <peterbartko132@gmail.com>
2022-04-23 17:37:04 +0200

    bug fixes

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,14 +7,26 @@ const colorize = score => {
 }
 
 export default function Result({score, setIntro, setShowQuiz, setScore}) {
+  
+  const replay = () => {
+    setShowQuiz(true)
+    setScore(0)
+  }
+  
+  const quit = () => {
+    setShowQuiz(true)
+    setIntro(false)
+    setScore(0)
+  }
+
   return (
     <div className="w-[25rem] pt-4 pb-7 bg-white rounded shadow-xl flex items-center justify-center flex-col gap-3 py-3 animate-appear">
       <FaCrown size={100} color={colorize(score).color} />
       <h2 className="font-bold text-xl">You've completed the Quiz</h2>
       <h3 className="font-bold text-md flex gap-1">You got <p style={colorize(score)}>{score}</p> out of 5</h3>
       <div className="space-x-4">
-        <button onClick={() => {setShowQuiz(true); setScore(0)}} className="btn">Replay Quiz</button>
-        <button onClick={() => {setShowQuiz(true); setIntro(false)}} className="btn bg-white text-blue-500 border-2 py-[.31rem] px-[14px] border-blue-500">Quit Quiz</button>
+        <button onClick={replay} className="btn">Replay Quiz</button>
+        <button onClick={quit} className="btn bg-white text-blue-500 border-2 py-[.31rem] px-[14px] border-blue-500">Quit Quiz</button>
       </div>      
     </div>
   )
